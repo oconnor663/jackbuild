6792ec2e4d425468228b5b854e86dab0ed58425c
Jack O'Connor <oconnor663@gmail.com>
2025-01-02 20:16:15 -0800

    working build.sh

---
+++ c_bin/build.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+set -e -u -o pipefail
+
+HERE="$(realpath "$(dirname "$BASH_SOURCE")")"
+
+set -x
+
+cd "$HERE/../rust_lib"
+cargo build
+
+cd "$(mktemp -d)"
+pwd
+cp "$HERE/main.c" .
+mkdir c_lib
+cp "$HERE/../c_lib/lib.h" c_lib/
+mkdir rust_lib
+cbindgen --lang=c "$HERE/../rust_lib" > rust_lib/lib.h
+gcc \
+    main.c \
+    "$HERE/../c_lib/lib.c" \
+    "$HERE/../rust_lib/target/debug/librust_lib.a" \
+    -o "$HERE/a.out"
--- c_bin/main.c
+++ c_bin/main.c
@@ -1,8 +1,10 @@
-#include "c_lib.h"
-#include "rust_lib.h"
+#include <stdio.h>
+
+#include "c_lib/lib.h"
+#include "rust_lib/lib.h"
 
 int main() {
   printf("C binary!\n");
-  printf("%s\n", get_rust_fn());
-  printf("%s\n", get_c_fn());
+  printf("%s\n", get_rust_str());
+  printf("%s\n", get_c_str());
 }
